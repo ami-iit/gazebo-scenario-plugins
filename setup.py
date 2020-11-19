897938e2c30742f4021bda3c5997d2eab8704c7f
Diego Ferigo <diego.ferigo@iit.it>
2020-11-19 18:30:30 +0100

    Style edits in setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from pathlib import Path
 from setuptools import setup, find_packages
 from cmake_build_extension import BuildExtension, CMakeExtension
 
-# Read the contents of your README file
-with open(Path(__file__).parent.absolute() / 'README.md', encoding='utf-8') as f:
+# Read the contents of the README file
+with open(Path(__file__).parent.absolute() / "README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='gazebo-scenario-plugins',
+    name="gazebo-scenario-plugins",
     author="Diego Ferigo",
     author_email="diego.ferigo@iit.it",
     description="Plugins for the Ignition Gazebo simulator implemented with ScenarI/O",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/dic-iit/gazebo-scenario-plugins',
+    url="https://github.com/dic-iit/gazebo-scenario-plugins",
     keywords="gazebo ignition simulation robotics plugin",
     license="LGPL",
-    platforms=['linux'],
+    platforms=["linux"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Operating System :: POSIX :: Linux",
@@ -33,8 +33,8 @@ setup(
     use_scm_version=dict(local_scheme="dirty-tag"),
     python_requires='>=3.8',
     install_requires=[
-        'gym-ignition',
-        'dataclasses',
+        "gym-ignition",
+        "dataclasses",
     ],
     setup_requires=[
         "setuptools_scm",
